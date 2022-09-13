35bdb7d0559485a7c8e70b4bf427a1919f281b84
Tsunami <2385942202@qq.com>
2022-09-13 21:59:31 +0200

    add dataset

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' vertices and paths of a graph
+#'
+#' A dataset containing the vertices and paths of a graph named wiki_graph
+#'
+#' @format A data frame contain 3 vectors,v1,v2 are pairs of vertices,and(v1,v2) is the edges.w are paths of the edges.  
+#' \describe{
+#'   \item{v1}{vertices}
+#'   \item{v2}{vertices}
+#'   \item{w}{paths of the edges}
+#'   ...
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)}
+"wiki_graph"
